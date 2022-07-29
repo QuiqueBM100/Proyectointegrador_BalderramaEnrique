a09b3d5ce58eafc3e8f0ef97c20045e46a1054a1
QuiqueBM100 <107310882+QuiqueBM100@users.noreply.github.com>
2022-07-29 19:01:16 -0300

    Creacion de carpetas FrontEnd y BackEnd
    Se crearon las carpetas se puso su correspondiente contenido para presentacion

---
+++ PI_BalderramaEnrique/BackEnd/src/main/java/com/portfolio/lebb/LebbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LebbApplication.class, args);
+	}
+
+}
---
+++ PI_BalderramaEnrique/BackEnd/src/test/java/com/portfolio/lebb/LebbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LebbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
